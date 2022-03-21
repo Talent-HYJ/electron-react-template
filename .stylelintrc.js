c8d7ca0d4554ff9d5e360620eaff093dce328cdc
heyuanjie <179708009@qq.com>
2022-03-21 17:56:20 +0800

    feat: 添加stylelint校验和格式化

---
+++ .stylelintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-prettier'
+  ],
+  plugins: ['stylelint-order'],
+  includes: ['*.less'],
+  rules: {
+    'color-no-invalid-hex': true, // 禁止无效的 16 进制颜色。
+    'font-family-no-duplicate-names': true, // 禁止重复的字体族名称
+    'font-family-no-missing-generic-family-keyword': true, // 禁止在字体族名称列表中缺少通用字体族关键字
+    // 'function-calc-no-invalid': false, // 禁止在 calc 函数中使用无效表达式。
+    'function-calc-no-unspaced-operator': true, // 禁止在 calc 函数中使用没有间隔的运算符
+    'string-no-newline': true, // 禁止字符串中的(未转义)换行符。
+    'unit-no-unknown': true, // 禁止未知的单位。
+    'property-no-unknown': [
+      // 禁止未知的属性。
+      true,
+      {
+        ignoreProperties: ['composes'] // 忽略 composes 属性
+      }
+    ],
+    'declaration-block-no-duplicate-properties': true, // 禁止声明块的重复属性
+    'selector-id-pattern': '^[a-z][a-zA-Z0-9]+$', // 指定 ID 选择器的模式。
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]|^ant-', // 指定 类 选择器的模式。
+    'selector-pseudo-class-no-unknown': [
+      // 禁止未知的伪类选择器。
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ],
+    'selector-pseudo-element-no-unknown': true, //禁止未知的伪元素选择器。
+    'selector-type-no-unknown': true, // 禁止未知的类型选择器。
+    'selector-pseudo-class-case': 'lower',
+    'selector-type-case': 'lower', // 指定类型选择器的大小写（可自动修复）
+    'media-feature-name-no-unknown': true, // 禁止未知的媒体功能名
+    'at-rule-no-unknown': true, // 禁止未知的@规则。
+    'comment-no-empty': true, // 禁止空注释。
+    'no-descending-specificity': true, //禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器。
+    'no-duplicate-at-import-rules': true, // 禁止在样式表中使用重复的 @import 规则。
+    'no-duplicate-selectors': true, // 禁止样式表中的重复选择器
+    'no-empty-source': true, // 禁止空源码
+    'no-extra-semicolons': true, // 禁止额外的分号（可自动修复）。
+    'string-quotes': 'single', // 指定字符串使用单引号或双引号（可自动修复）。
+    'property-case': 'lower', // 指定属性的大小写（可自动修复）。
+    'max-empty-lines': 1, // 限制相邻空行的数量。
+    // 'no-eol-whitespace': true,                       // issue: #3842
+    'function-name-case': 'lower',
+    indentation: 4 // 指定缩进。
+  }
+};
--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,108 +1,122 @@
-
 const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
-const lessModuleRegex = /\.module\.less$/
-process.env.PUBLIC_URL = path.resolve(__dirname,'../public')
-module.exports ={
-  entry:'./index.tsx',
+const lessModuleRegex = /\.module\.less$/;
+process.env.PUBLIC_URL = path.resolve(__dirname, '../public');
+module.exports = {
+  entry: './index.tsx',
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: '[name][contenthash:10].js',
+    filename: '[name][contenthash:10].js'
   },
-  stats: "minimal",
-  devtool:'cheap-source-map',
+  stats: 'minimal',
+  devtool: 'cheap-source-map',
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    mainFields: ['jsnext:main', 'module','browser', 'main'],
-    extensions: [".ts", ".tsx", ".js", ".json",'.less','.css']
-},
-  module:{
+    mainFields: ['jsnext:main', 'module', 'browser', 'main'],
+    extensions: ['.ts', '.tsx', '.js', '.json', '.less', '.css']
+  },
+  module: {
     rules: [
-    {
-      test: /\.(js|mjs|jsx|ts|tsx)$/,
-      exclude:  /node_modules/,
-       enforce: "pre",
-      use: {
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true,
-          cacheCompression: false,
-          sourceMaps: true,
-          inputSourceMap: true,
-          presets: [["@babel/preset-react",{
-            "runtime":'automatic'
-          }],
-          ['@babel/preset-env',{
-            useBuiltIns:'usage',
-            corejs:{
-              version:2
-            },
-          }],['@babel/preset-typescript']],
-        },
-      }
-    },
-    {
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader",{
-          loader: "postcss-loader",
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: {
+          loader: 'babel-loader',
           options: {
-            postcssOptions: {
-              plugins: [
-                require('autoprefixer')({
-                  "overrideBrowserslist": [
-                    "> 1%",
-                    "last 7 versions",
-                    "not ie <= 8",
-                    "ios >= 8",
-                    "android >= 4.0"
-                  ]
-                })
-              ]
+            cacheDirectory: true,
+            cacheCompression: false,
+            sourceMaps: true,
+            inputSourceMap: true,
+            presets: [
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic'
+                }
+              ],
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: {
+                    version: 2
+                  }
+                }
+              ],
+              ['@babel/preset-typescript']
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer')({
+                    overrideBrowserslist: [
+                      '> 1%',
+                      'last 7 versions',
+                      'not ie <= 8',
+                      'ios >= 8',
+                      'android >= 4.0'
+                    ]
+                  })
+                ]
+              }
             }
           }
-        
-      }],
-    },
-    {
-      test: /\.less$/i,
-      exclude:lessModuleRegex,
+        ]
+      },
+      {
+        test: /\.less$/i,
+        exclude: lessModuleRegex,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: lessModuleRegex,
         use: [
           'style-loader',
           {
             loader: 'css-loader',
-          },
-          {
-            loader:'less-loader',
-          }
-        ],
-    },
-    {
-      test: lessModuleRegex,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options:{
-              modules:true
+            options: {
+              modules: true
             }
           },
           {
-            loader:'less-loader',
+            loader: 'less-loader'
           }
-        ],
-    },
-    {
-      test:/\.(jpg|png|gif|bmp|jpeg|ico)$/ ,
-      use:['url-loader'],
-      generator:{
-        filename:'static/[name].[ext]'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|bmp|jpeg|ico)$/,
+        use: ['url-loader'],
+        generator: {
+          filename: 'static/[name].[ext]'
+        }
       }
-     }
-  ],
+    ]
   },
   plugins: [
-  new ESLintPlugin(),
-  new FriendlyErrorsWebpackPlugin()
-],
-}
+    new ESLintPlugin({
+      fix: true,
+      cache: true
+    }),
+    new FriendlyErrorsWebpackPlugin()
+  ]
+};
