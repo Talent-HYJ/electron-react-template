a3a87b11cda6afcc6ea90bf6b8c4464bad6b805b
heyuanjie <179708009@qq.com>
2022-11-18 14:57:08 +0800

    chore:移除prettier

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,57 +1,56 @@
 module.exports = {
-  productName: 'Electron React',
-  appId: 'your.id',
+  productName: "Electron React",
+  appId: "your.id",
   asar: true,
   mac: {
-    category: 'your.app.category.type',
-    icon: 'build/favicon.icns'
+    category: "your.app.category.type",
+    icon: "build/favicon.icns",
   },
   win: {
-    icon: 'build/favicon.png'
+    icon: "build/favicon.png",
   },
   nsis: {
     oneClick: false,
-    language: '2052',
+    language: "2052",
     deleteAppDataOnUninstall: true,
-    allowToChangeInstallationDirectory: true
+    allowToChangeInstallationDirectory: true,
   },
   dmg: {
-    icon: 'build/favicon.icns',
+    icon: "build/favicon.icns",
     iconSize: 128,
     contents: [
       {
         x: 380,
         y: 180,
-        type: 'link',
-        path: '/Applications'
+        type: "link",
+        path: "/Applications",
       },
       {
         x: 130,
         y: 180,
-        type: 'file'
-      }
+        type: "file",
+      },
     ],
     window: {
       width: 540,
-      height: 380
-    }
+      height: 380,
+    },
   },
   files: [
-    'build/**/*',
-    'electron/**',
-    'build_electron/main.jsc',
-    'build_electron/preload.js',
-    '!electron/main.js',
-    '!electron/preload.js',
-    '!**/node_modules/**',
-    'node_modules/electron-is-dev/**',
-    'node_modules/bytenode/**',
-    // eslint-disable-next-line prettier/prettier, no-useless-escape
-    'node_modules/@electron/**'
+    "build/**/*",
+    "electron/**",
+    "build_electron/main.jsc",
+    "build_electron/preload.js",
+    "!electron/main.js",
+    "!electron/preload.js",
+    "!**/node_modules/**",
+    "node_modules/electron-is-dev/**",
+    "node_modules/bytenode/**",
+    "node_modules/@electron/**",
   ],
   directories: {
-    buildResources: './public',
-    output: './package'
+    buildResources: "./public",
+    output: "./package",
   },
-  extends: null
+  extends: null,
 };
